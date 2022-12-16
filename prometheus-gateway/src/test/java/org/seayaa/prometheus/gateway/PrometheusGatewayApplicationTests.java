4b5a844ca9f71dd61b63e40706906925317cd4d2
Seayaa <dongdahai1004@163.com>
2022-12-16 22:49:53 +0800

    【提交类型】初始提交 【提交内容】gateway

---
+++ prometheus-gateway/src/test/java/org/seayaa/prometheus/gateway/PrometheusGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.seayaa.prometheus.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
